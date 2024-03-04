bfb1080363d2c5a7359fd912f358ccdda878b904
Robersonico <jrogutierrez@gmail.com>
2024-03-04 15:57:57 -0300

    REST API to FL0

---
+++ schemas/movies.js
@@ -0,0 +1,34 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+    title: z.string({
+        invalid_type_error: 'Movie Title must be string', //Ejemplo de texto error de tipeo
+        required_error: 'Mobie Title is required'  //EJemplo de requerimiento
+    }),
+    year: z.number().int().min(1900).max(2024), //Se puede agregar varios requerimientos
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10).default(5),
+    poster: z.string().url({//Se puede agregar mas como endwith(jpg)
+        message: 'Poster must be a valid url' 
+    }),
+     genre: z.array(
+  z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+{
+  required_error: 'Movie genre is required.',
+  invalid_type_error: 'Movie genre must be an array of enum Genre'
+}
+)
+})
+
+ function validateMovie(input){
+    return movieSchema.safeParse(input)
+ }
+
+ function validatePartialMovie(input){
+    return movieSchema.partial().safeParse(input)
+ }
+ module.exports = {
+    validateMovie,
+    validatePartialMovie
+ }
